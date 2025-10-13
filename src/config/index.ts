3bced9151db787e4a56f68b758bc8f3054877106
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-10-13 16:54:16 +0330

    feat: initial commit

---
+++ src/config/index.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Config } from "@fullstacksjs/config";
+import { env } from "node:process";
+
+const schema = new Config({
+  bot: Config.object({
+    token: Config.string().required(),
+    chatId: Config.number().required(),
+    topicId: Config.number(),
+  }).required(),
+
+  github: Config.object({
+    token: Config.string().required(),
+  }).required(),
+});
+
+const rawConfig = schema
+  .parse({
+    bot: {
+      token: env.BOT_TOKEN,
+      chatId: env.BOT_CHAT_ID,
+      topicId: env.BOT_TOPIC_ID,
+    },
+    github: {
+      token: env.GITHUB_TOKEN,
+    },
+  })
+  .getAll();
+
+// NOTE: This is a temporary workaround until the issue with undefined fields gets fixed.
+export const config = {
+  bot: {
+    token: { ...rawConfig.bot.token, value: rawConfig.bot.token.value! },
+    chatId: { ...rawConfig.bot.chatId, value: rawConfig.bot.chatId.value! },
+    topicId: rawConfig.bot.topicId.value,
+  },
+  github: {
+    token: { ...rawConfig.github.token, value: rawConfig.github.token.value! },
+  },
+} as const;
---
+++ src/types/env.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    // Telegram Bot variables
+    BOT_TOKEN?: string;
+    BOT_CHAT_ID?: string;
+    BOT_TOPIC_ID?: string;
+
+    // GitHub Application variables
+    GITHUB_TOKEN?: string;
+  }
+}
