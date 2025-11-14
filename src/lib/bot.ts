f70be6ad5f67ae64791e064491f64cbaf33b8b65
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-11-14 16:22:04 +0330

    chore: update bot types and make i18n public

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -11,26 +11,36 @@ import { config } from "@/config";
 /** BotContext is the context passed to the update handlers after passing all middlewares. */
 export type BotContext = Context & I18nFlavor;
 
+interface AnnounceChat {
+  /** Numeric id of the chat */
+  chatId: number;
+  /** Optional. Numeric id of the chat's topic */
+  topicId?: number | undefined;
+}
+
 /** BotOptions is the constructor options of Bot class */
 interface BotOptions {
   /** Telegram bot's token */
   token: string;
 
   /** Default channel to announce updates in */
-  announceChat: {
-    /** Numeric id of the chat */
-    chatId: number;
-    /** Optional. Numeric id of the chat's topic */
-    topicId?: number | undefined;
-  };
+  announceChat: AnnounceChat;
 }
 
 /**
  * Bot extends GrammY's Bot by doing the bootstrap steps in constructor level.
  */
 export class Bot extends GrammyBot<BotContext> {
+  i18n = new I18n<BotContext>({
+    defaultLocale: "en",
+    directory: "locales",
+    fluentBundleOptions: {
+      useIsolating: false,
+    },
+  });
+
   /** Default chat used to announce messages in */
-  announceChat: BotOptions["announceChat"];
+  private announceChat: AnnounceChat;
 
   /**
    * @param options bot options required to make the bot work.
@@ -39,15 +49,7 @@ export class Bot extends GrammyBot<BotContext> {
     super(token);
     this.announceChat = announceChat;
 
-    const i18n = new I18n<BotContext>({
-      defaultLocale: "en",
-      directory: "locales",
-      fluentBundleOptions: {
-        useIsolating: false,
-      },
-    });
-
-    this.use(i18n);
+    this.use(this.i18n);
     this.use(
       limit({
         limit: 10,
@@ -68,9 +70,9 @@ export class Bot extends GrammyBot<BotContext> {
    * @param chatId ID of the chat for the message to get announced in. default: `config.bot.chatId`
    * @param topicId ID of the chat topic for the message to get announced in. default: `config.bot.topicId`
    */
-  public async announce(text: string, chatId?: number, topicId?: number) {
-    await this.api.sendMessage(chatId ?? this.announceChat.chatId, text, {
-      direct_messages_topic_id: topicId ?? this.announceChat.topicId,
+  public async announce(text: string, chat?: AnnounceChat) {
+    await this.api.sendMessage(chat?.chatId ?? this.announceChat.chatId, text, {
+      direct_messages_topic_id: chat?.topicId ?? this.announceChat.topicId,
       parse_mode: "MarkdownV2",
     });
   }
