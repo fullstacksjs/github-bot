1666cb8442e35677c6a8cce60321ae1e0f8c26e3
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-11-14 16:22:38 +0330

    feat: fully implement api for webhook listeners

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,43 @@
+import { serve } from "@hono/node-server";
+import { webhookCallback } from "grammy";
+import { Hono } from "hono";
+
+import { bot } from "@/bot";
+
 import { commands } from "./commands";
-import { bot } from "./lib/bot";
+import { config } from "./config";
+import { webhooks } from "./lib/github-webhooks";
 
-await commands.setCommands(bot);
+async function setupBot() {
+  bot.use(commands);
 
-bot.use(commands);
+  await commands.setCommands(bot);
 
-bot.start({
-  onStart(botInfo) {
-    // eslint-disable-next-line no-console
-    console.log(`Bot started as @${botInfo.username}`);
-  },
-});
+  await bot.api.setWebhook(config.bot.webhookUrl, {
+    allowed_updates: ["message"],
+    secret_token: config.bot.webhookSecret,
+  });
+}
+
+async function startApi() {
+  const api = new Hono();
+
+  api.use(`/api/webhook/telegram`, webhookCallback(bot, "hono", { secretToken: config.bot.webhookSecret }));
+
+  api.post(`/api/webhook/github`, async (ctx) => {
+    await webhooks.verifyAndReceive({
+      id: ctx.req.header("x-request-id") ?? "",
+      name: ctx.req.header("x-github-event") ?? "",
+      signature: ctx.req.header("x-hub-signature") ?? "",
+      payload: await ctx.req.text(),
+    });
+
+    return ctx.text("Accepted", 202);
+  });
+
+  return serve({ fetch: api.fetch, port: config.api.port });
+}
+
+await setupBot();
+
+await startApi();
