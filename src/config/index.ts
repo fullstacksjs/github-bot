f73a2bc387e816d6eb941e0d071099f519fc5e9c
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-11-14 15:51:08 +0330

    feat: add api & webhook config fields

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,16 +2,23 @@ import { Config } from "@fullstacksjs/config";
 import { env } from "node:process";
 
 const schema = new Config({
+  api: Config.object({
+    port: Config.number({ default: 8080 }),
+  }),
+
   bot: Config.object({
     token: Config.string().required(),
     chatId: Config.number().required(),
     topicId: Config.number(),
     adminIds: Config.array(Config.number({ coerce: true })),
+    webhookUrl: Config.string().required(),
+    webhookSecret: Config.string().required(),
   }),
 
   github: Config.object({
     token: Config.string().required(),
     orgName: Config.string().required(),
+    webhookSecret: Config.string().required(),
   }),
 
   database: Config.object({
@@ -21,15 +28,21 @@ const schema = new Config({
 
 export const config = schema
   .parse({
+    api: {
+      port: env.API_PORT,
+    },
     bot: {
       token: env.BOT_TOKEN,
       chatId: env.BOT_CHAT_ID,
       topicId: env.BOT_TOPIC_ID,
       adminIds: env.BOT_ADMIN_IDS?.split(",").map((v) => parseInt(v.trim(), 10)),
+      webhookUrl: env.BOT_WEBHOOK_URL,
+      webhookSecret: env.BOT_WEBHOOK_SECRET,
     },
     github: {
       token: env.GITHUB_TOKEN,
       orgName: env.GITHUB_ORG_NAME,
+      webhookSecret: env.GITHUB_WEBHOOK_SECRET,
     },
     database: {
       filePath: env.DB_FILE_PATH,
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,13 +1,20 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    // API variables
+    API_PORT?: string;
+
     // Telegram Bot variables
     BOT_TOKEN?: string;
     BOT_CHAT_ID?: string;
     BOT_TOPIC_ID?: string;
+    BOT_ADMIN_IDS?: string;
+    BOT_WEBHOOK_URL?: string;
+    BOT_WEBHOOK_SECRET?: string;
 
     // GitHub Application variables
     GITHUB_TOKEN?: string;
     GITHUB_ORG_NAME?: string;
+    GITHUB_WEBHOOK_SECRET?: string;
 
     // Database Application variables
     DB_FILE_PATH?: string;
