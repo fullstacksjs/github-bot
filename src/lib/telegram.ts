e23d6c41bf0f8618c4777bb6ad1d249d470dc8ab
ehsanKey <ehsanKeyhani82@gmail.com>
2025-11-24 02:28:57 +0330

    feat: remove isTelegramId function

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -1,8 +1,3 @@
 export function cleanTelegramUsername(username: string): string {
   return username.startsWith("@") ? username.slice(1) : username;
 }
-
-export function isTelegramId(arg: number | string): boolean {
-  const num = Number(arg);
-  return /^\d+$/.test(String(arg)) && num > 0 && num <= 2147483647;
-}
