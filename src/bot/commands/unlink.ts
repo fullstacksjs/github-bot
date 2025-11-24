c4ca5edf70081160357b2e5c84432eca75ee603c
Alireza Safaierad <alireza.safaierad@gmail.com>
2025-11-24 23:22:30 +0100

    refactor: migrate unlink command

--- src/bot/commands/unlink.ts
+++ src/bot/commands/unlink.ts
@@ -1,26 +1,19 @@
-import { Command } from "@grammyjs/commands";
 import { config } from "#config";
-import { db, schema } from "#db";
-import { CommandParser, zs } from "#telegram";
+import { db, schema as s } from "#db";
+import { createCommand, zs } from "#telegram";
 import { eq } from "drizzle-orm";
 import z from "zod";
 
 import type { BotContext } from "../bot.ts";
 
-export async function unlinkHandler(ctx: BotContext) {
-  if (!ctx.message?.text) return;
+const schema = z.object({ tgUsername: zs.tgUsername });
 
+export async function handler(ctx: BotContext) {
   if (!config.bot.adminIds.includes(ctx.message.from.id)) {
     return await ctx.md.replyToMessage(ctx.t("insufficient_permissions"));
   }
 
-  const parser = CommandParser("/unlink $tgUsername", z.object({ tgUsername: zs.tgUsername }));
-  const { success, data } = parser(ctx.message.text);
-
-  if (!success) {
-    return await ctx.md.replyToMessage(ctx.t("cmd_unlink_help"));
-  }
-  const { tgUsername } = data;
+  const { tgUsername } = ctx.args;
 
   const existingContributor = await db.query.contributors.findFirst({
     where: (f, o) => o.eq(f.tgUsername, tgUsername),
@@ -30,12 +23,16 @@ export async function unlinkHandler(ctx: BotContext) {
     return await ctx.md.replyToMessage(ctx.t("cmd_unlink_not_found"));
   }
 
-  await db.update(schema.contributors).set({ tgUsername: null }).where(eq(schema.contributors.tgUsername, tgUsername));
+  await db.update(s.contributors).set({ tgUsername: null }).where(eq(s.contributors.tgUsername, tgUsername));
 
   return await ctx.md.replyToMessage(ctx.t("cmd_unlink"));
 }
 
-export const cmdUnLink = new Command<BotContext>("unlink", "🛡 UnLink Telegram and GitHub accounts").addToScope(
-  { type: "chat_administrators", chat_id: config.bot.chatId },
-  unlinkHandler,
-);
+export const cmdUnlink = createCommand({
+  template: "unlink $tgUsername",
+  description: "🛡 UnLink Telegram and GitHub accounts",
+  handler,
+  schema,
+  helpMessage: (t) => t("cmd_unlink_help"),
+  scopes: [{ type: "chat_administrators", chat_id: config.bot.chatId }],
+});
