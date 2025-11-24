7c78f7386d96646a25e0fff60a3448b5cd989843
Alireza Safaierad <alireza.safaierad@gmail.com>
2025-11-24 23:14:29 +0100

    refactor: migrate help command

--- src/bot/commands/help.ts
+++ src/bot/commands/help.ts
@@ -1,17 +1,16 @@
-import { Command } from "@grammyjs/commands";
 import { config } from "#config";
+import { createCommand } from "#telegram";
 
 import type { BotContext } from "../bot.ts";
 
-export async function helpHandler(ctx: BotContext) {
-  if (!ctx.message) return;
-
+export async function handler(ctx: BotContext) {
   const isAdmin = config.bot.adminIds.includes(ctx.message.from.id);
 
   return await ctx.replyToMessage(ctx.t(isAdmin ? "cmd_help_admin" : "cmd_help"));
 }
 
-export const cmdHelp = new Command<BotContext>("help", "About This Bot").addToScope(
-  { type: "chat", chat_id: config.bot.chatId },
-  helpHandler,
-);
+export const cmdHelp = createCommand({
+  template: "help",
+  description: "About This Bot",
+  handler,
+});
