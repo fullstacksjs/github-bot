c42ea5b06e893d42600f2dab5478f9d8a9d49faf
Alireza Safaierad <alireza.safaierad@gmail.com>
2025-11-27 23:48:37 +0100

    feat: add reporting

---
+++ src/bot/middleware/logger.ts
@@ -0,0 +1,65 @@
+/* eslint-disable no-console */
+import type { BotContext } from "#bot";
+
+import { config } from "#config";
+import { GrammyError, HttpError } from "grammy";
+
+export const logger = async (ctx: BotContext, next: () => Promise<unknown>) => {
+  ctx.logger = {
+    log: async (message: string) => {
+      console.log(message);
+      const reportId = config.bot.reportChatId;
+      if (!reportId) return;
+
+      return ctx.api.sendMessage(reportId, message, { parse_mode: "HTML" });
+    },
+    error: async (message: string) => {
+      console.log("Report for", config.bot.reportChatId);
+
+      console.error(message);
+      const reportId = config.bot.reportChatId;
+      if (!reportId) return;
+
+      return ctx.api.sendMessage(reportId, message, { parse_mode: "HTML" });
+    },
+  };
+
+  ctx.report = async (e: unknown) => {
+    let message = "";
+    const update = ctx.update.message;
+    const link = update?.from.username
+      ? `@${update.from.username}`
+      : `<a href="tg://user?id=${update?.from.id}">${update?.from.first_name}</a>`;
+
+    message += [
+      "<b>Error:</b>",
+      `Command: <code>${update?.text}</code>`,
+      `Sender Name: <code>${link}</code>`,
+      "",
+      "<b>Message:</b>",
+      "",
+    ].join("\n");
+
+    if (e instanceof GrammyError) {
+      message += `<pre>${e.description}</pre>\n`;
+    } else if (e instanceof HttpError) {
+      message += `<pre>${e}</pre>\n`;
+    } else {
+      message += `<pre>${e}</pre>\n`;
+    }
+
+    message += `\n#error`;
+
+    ctx.logger.error(message);
+  };
+
+  return next();
+};
+
+export interface LoggerContext {
+  logger: {
+    log: (log: string) => Promise<unknown> | undefined;
+    error: (error: string) => Promise<unknown> | undefined;
+  };
+  report: (error: unknown) => Promise<unknown> | undefined;
+}
