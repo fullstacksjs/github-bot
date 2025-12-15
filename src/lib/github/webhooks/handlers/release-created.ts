a0be9594ecce642b56362fcb3a70dc2565fa4e7e
Alireza Safaierad <alireza.safaierad@gmail.com>
2025-12-15 18:06:44 +0100

    fix: fix release note limit

--- src/lib/github/webhooks/handlers/release-created.ts
+++ src/lib/github/webhooks/handlers/release-created.ts
@@ -9,13 +9,16 @@ export const releaseCreatedCallback: HandlerFunction<"release.created", unknown>
   const repo = event.payload.repository;
   const release = event.payload.release;
   const repoHashtag = getRepoHashtag(repo.name);
+
   if (release.body) {
+    const releaseNotesPreview = release.body.length > 2000 ? `${release.body.slice(0, 2000)}\n...` : release.body;
+
     await bot.announce(
       botText("e_release_created_with_notes", {
         repoName: escapeHtml(repo.name),
         releaseTag: escapeHtml(release.tag_name),
         releaseUrl: escapeHtml(release.html_url),
-        notes: release.body,
+        notes: releaseNotesPreview,
         repoHashtag,
       }),
       { link_preview_options: { prefer_small_media: true, url: release.html_url } },
